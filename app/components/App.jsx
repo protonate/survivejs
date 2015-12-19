70568fb5e2a7c5bcd7aff0870ee95be174562c7e
protonate <n8guisinger@gmail.com>
2015-12-19 07:00:00 -0800

    add Notes componenent, move App model to component state

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,6 +1,36 @@
+import uuid from 'node-uuid';
 import React from 'react';
-import Note from './Note.jsx';
+import Note from './Notes.jsx';
 
-export default () => {
-  return <Note />
+
+export default class App extends React.Component {
+  constructor (props) {
+    this.super(props);
+
+    this.state = {
+      notes: [
+        {
+          id: uuid.v4(),
+          task: 'Learn Webpack'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Learn React'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Do laundries'
+        }
+      ]
+    };
+  }
+  render() {
+    const notes = this.state.notes;
+
+    return (
+      <div>
+        <Notes items={notes} />
+      </div>
+    )
+  }
 }
